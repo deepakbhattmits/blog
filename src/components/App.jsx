24b7632fb81be7b2606f67bbd4e0e9fcc2b58a2a
deepakbhattmits <deepakbhattmits@gmail.com>
2022-01-12 08:01:55 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,8 @@
 import React from 'react';
 import PostList from './PostList';
 import RegisterForm from './RegisterFrom';
-const App = () => {
-    
-    return (
-        <div className="ui container">
+const App = () => <div className="ui container">
             <RegisterForm />
            <PostList /> 
         </div>
-    );
-
-}
 export default App;
